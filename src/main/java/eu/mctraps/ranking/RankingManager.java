ea4d6fa82ec574f66be371f1d48dd9e06cfa6973
KajmaczeK <kajmaczek@kajmaczek.pl>
2017-09-23 15:48:47 +0200

    /ranking

--- src/main/java/eu/mctraps/ranking/RankingManager.java
+++ src/main/java/eu/mctraps/ranking/RankingManager.java
@@ -1,5 +1,6 @@
 package eu.mctraps.ranking;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RankingManager {
@@ -11,5 +12,83 @@ public class RankingManager {
             return false;
         }
         return true;
+    }
+
+    public static boolean setKills(String player, int number, Ranking plugin) {
+        try {
+            plugin.statement.executeUpdate("UPDATE " + plugin.rTable + " SET kills='" + number + "' WHERE player='" + player + "'");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean setRank(String player, int number, Ranking plugin) {
+        try {
+            plugin.statement.executeUpdate("UPDATE " + plugin.rTable + " SET rank='" + number + "' WHERE player='" + player + "'");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static int getDeaths(String player, Ranking plugin) {
+        try {
+            ResultSet r = plugin.statement.executeQuery("SELECT * FROM " + plugin.rTable + " WHERE player='" + player + "'");
+            if(r.next()) {
+                return r.getInt("deaths");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return -1;
+    }
+
+    public static int getKills(String player, Ranking plugin) {
+        try {
+            ResultSet r = plugin.statement.executeQuery("SELECT * FROM " + plugin.rTable + " WHERE player='" + player + "'");
+            if(r.next()) {
+                return r.getInt("kills");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return -1;
+    }
+
+    public static int getRank(String player, Ranking plugin) {
+        try {
+            ResultSet r = plugin.statement.executeQuery("SELECT * FROM " + plugin.rTable + " WHERE player='" + player + "'");
+            if(r.next()) {
+                return r.getInt("rank");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return -1;
+    }
+
+    public static boolean exists(String player, Ranking plugin) {
+        try {
+            ResultSet r = plugin.statement.executeQuery("SELECT COUNT(*) FROM " + plugin.rTable + " WHERE player='" + player + "'");
+            int count = 0;
+            while(r.next()) {
+                count = r.getInt(1);
+            }
+
+            if(count > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
--- src/main/java/eu/mctraps/ranking/commands/RankingCommand.java
+++ src/main/java/eu/mctraps/ranking/commands/RankingCommand.java
@@ -1,6 +1,7 @@
 package eu.mctraps.ranking.commands;
 
 import eu.mctraps.ranking.Ranking;
+import eu.mctraps.ranking.RankingManager;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -18,7 +19,41 @@ public class RankingCommand implements CommandExecutor {
         if(args.length == 0) {
             if(sender instanceof Player) {
                 Player p = (Player)sender;
+                String name = p.getName();
+                int kills = RankingManager.getKills(name, plugin);
+                int deaths = RankingManager.getDeaths(name, plugin);
+                int rank = RankingManager.getRank(name, plugin);
+                float kd = kills / deaths;
 
+                sender.sendMessage("§8§m--------§8§l« §9§lTWOJE STATYSTYKI §8§l»§8§m--------");
+                sender.sendMessage("§8● §7Ranking §8§l» §6" + rank);
+                sender.sendMessage("§8● §7Zabojstwa §8§l» §6" + kills);
+                sender.sendMessage("§8● §7Smierci §8§l» §6" + deaths);
+                sender.sendMessage("§8● §7K/D §8§l» §6" + kd);
+                sender.sendMessage("§8§m-----------------------------------------");
+            } else {
+                sender.sendMessage("§cPoprawne uzycie: §7/" + label + " §c<nick>");
+            }
+        } else {
+            if(args[0].equalsIgnoreCase("top")) {
+                // ToDo: top
+            } else {
+                if(RankingManager.exists(args[0], plugin)) {
+                    String name = args[0];
+                    int kills = RankingManager.getKills(name, plugin);
+                    int deaths = RankingManager.getDeaths(name, plugin);
+                    int rank = RankingManager.getRank(name, plugin);
+                    float kd = kills / deaths;
+
+                    sender.sendMessage("§8§m--------§8§l« §9§lSTATYSTYKI§9: §7" + name + " §8§l»§8§m--------");
+                    sender.sendMessage("§8● §7Ranking §8§l» §6" + rank);
+                    sender.sendMessage("§8● §7Zabojstwa §8§l» §6" + kills);
+                    sender.sendMessage("§8● §7Smierci §8§l» §6" + deaths);
+                    sender.sendMessage("§8● §7K/D §8§l» §6" + kd);
+                    sender.sendMessage("§8§m-----------------------------------------");
+                } else {
+                    sender.sendMessage("§cBlad: §4taki gracz nie istnieje");
+                }
             }
         }
         return false;
